3c246c2ca746e4938a04d07e72aeca939b858f9b
Pascal Mainini <pascal.mainini@bfh.ch>
2013-07-17 13:48:23 +0200

    created initialisation-script for ca

---
+++ ca/initialise.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Generate ca-cert
+openssl req -new -batch -x509 -keyout private/cakey.pem -extensions req_ext -out cacert.pem -days 365 -config config/ca-certificate.conf
+
+# Generate Server-request
+openssl req -new -batch -config config/server.conf -out server-req.pem -keyout server-key.pem
+
+# Generate WebID-request
+openssl req -new -batch -config config/webid.conf -out webid-req.pem -keyout webid-key.pem
+
+# Sign certificates
+openssl ca -batch -config config/ca.conf -notext -out server-cert.pem -infiles server-req.pem
+openssl ca -batch -config config/ca.conf -notext -out webid-cert.pem -infiles webid-req.pem
