a529d44778445a1c00b2ebfe0df7dbd32f6e5d0f
Pascal Mainini <pascal.mainini@bfh.ch>
2013-07-17 16:08:00 +0200

    added creation of pkcs12

--- ca/initialise.sh
+++ ca/initialise.sh
@@ -12,3 +12,8 @@ openssl req -new -batch -config config/webid.conf -out webid-req.pem -keyout web
 # Sign certificates
 openssl ca -batch -config config/ca.conf -notext -out server-cert.pem -infiles server-req.pem
 openssl ca -batch -config config/ca.conf -notext -out webid-cert.pem -infiles webid-req.pem
+
+# Create WebID for broser...
+cp webid-cert.pem the-webid.pem
+cat webid-key.pem >>the-webid.pem
+openssl pkcs12 -export -in the-webid.pem -out the-webid.p12 -name "Justus Testus' WebID"
