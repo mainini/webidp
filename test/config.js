d8f77fff4c5e7b352a1e73d1c0acaf45f95b18b8
Pascal Mainini <pascal.mainini@bfh.ch>
2013-07-31 18:31:26 +0200

    initially added

---
+++ test/config.js
@@ -0,0 +1,28 @@
+/**
+ * @file Tests for config.js
+ * @copyright 2013 Berne University of Applied Sciences (BUAS) -- {@link http://bfh.ch}
+ * @author Pascal Mainini <pascal.mainini@bfh.ch>
+ * @version 0.0.1
+ *
+ * ! WARNING ! WARNING ! WARNING ! WARNING ! WARNING ! WARNING !
+ *
+ * THIS FILE HAS NO DEFINITIVE LICENSING INFORMATION.
+ * LICENSE IS SUBJECT OF CHANGE ANYTIME SOON - DO NOT DISTRIBUTE!
+ *
+ * ! WARNING ! WARNING ! WARNING ! WARNING ! WARNING ! WARNING !
+ */
+
+/*jshint node:true, bitwise:true, curly:true, immed:true, indent:2, latedef:true, newcap:true, noarg: true, noempty:true, nonew:true, quotmark:single, undef:true, unused: true, trailing:true, white:false */
+
+/***********************************************************
+ * Initialisation
+ **********************************************************/
+
+'use strict';
+
+var cfg = require('../src/config.js');
+
+console.log('IdUri           : ' + cfg.getIdUri('test'));
+console.log('IdUriFull       : ' + cfg.getIdUriFull('test'));
+console.log('ValidityStart   : ' + cfg.getValidityStart());
+console.log('ValidityEnd     : ' + cfg.getValidityEnd());
