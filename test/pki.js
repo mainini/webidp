77c0e6d610dab7a939a095bd7f6f0d5ad7c96756
Pascal Mainini <pascal.mainini@bfh.ch>
2013-09-26 13:16:08 +0200

    directly writing out certificates and keys, beautified and cleaned up

--- test/pki.js
+++ test/pki.js
@@ -14,44 +14,41 @@
 
 /*jshint node:true, bitwise:true, curly:true, immed:true, indent:2, latedef:true, newcap:true, noarg: true, noempty:true, nonew:true, quotmark:single, undef:true, unused: true, trailing:true, white:false */
 
-/***********************************************************
- * Initialisation
- **********************************************************/
-
 'use strict';
 
-var forge = require('node-forge'),
+var fs = require('fs'),
+  forge = require('node-forge'),
   pki = require('../src/pki.js');
 
-var webidKeys = forge.pki.rsa.generateKeyPair(1024);
-var webid = pki.createCertificate('test', 'Justus Testus', 'justus.testus@bfh.ch', webidKeys.publicKey, '01');
 
-var caKeys = forge.pki.rsa.generateKeyPair(1024);
+// Generate and save CA-certificate
+var caKeys = forge.pki.rsa.generateKeyPair(2048);
 var caSubject = [{ name: 'organizationName', value: 'Berne University of Applied Sciences' },
                { name: 'organizationalUnitName', value: 'Engineering and Information Technology' },
                { name: 'countryName', value: 'CH' },
                { name: 'commonName', value: 'BFH WebID CA' }];
-var start = new Date();
-var end = new Date();
-end.setFullYear(start.getFullYear() + 1);
-var caCert = pki.createCACertificate(caSubject, caKeys, start, end, 0, true);
+var caCert = pki.createCACertificate(caSubject, caKeys, '0', true);
 
-var serverKeys = forge.pki.rsa.generateKeyPair(1024);
+fs.writeFile('ca-cert.pem', forge.pki.certificateToPem(caCert));
+fs.writeFile('ca-key.pem', forge.pki.privateKeyToPem(caKeys.privateKey));
+
+
+// Generate and save server-certificate
+
+var serverKeys = forge.pki.rsa.generateKeyPair(2048);
 var serverSubject = [{ name: 'organizationName', value: 'Berne University of Applied Sciences' },
                { name: 'countryName', value: 'CH' },
                { name: 'commonName', value: 'localhost' }];
+var serverCert = pki.createServerCertificate(serverSubject, '127.0.0.1', serverKeys, '1', caCert, caKeys, true);
 
-var serverCert = pki.createServerCertificate(serverSubject, caSubject, '127.0.0.1', serverKeys, caKeys, start, end, 1, true);
+fs.writeFile('server-cert.pem', forge.pki.certificateToPem(serverCert));
+fs.writeFile('server-key.pem', forge.pki.privateKeyToPem(serverKeys.privateKey));
 
-console.log('WebID-certificate:');
-console.log(forge.pki.certificateToPem(webid));
-console.log('WebID private key:');
-console.log(forge.pki.privateKeyToPem(webidKeys.privateKey));
-console.log('CA-certificate:');
-console.log(forge.pki.certificateToPem(caCert));
-console.log('CA private key:');
-console.log(forge.pki.privateKeyToPem(caKeys.privateKey));
-console.log('Server-certificate:');
-console.log(forge.pki.certificateToPem(serverCert));
-console.log('Server private key:');
-console.log(forge.pki.privateKeyToPem(serverKeys.privateKey));
+
+// Generate and save WebID-certificate
+
+var webidKeys = forge.pki.rsa.generateKeyPair(2048);
+var webidCert = pki.createWebIDCertificate('test', 'Justus Testus', 'justus.testus@bfh.ch', webidKeys, '2', caCert, caKeys, true);
+
+fs.writeFile('webid-cert.pem', forge.pki.certificateToPem(webidCert));
+fs.writeFile('webid-key.pem', forge.pki.privateKeyToPem(webidKeys.privateKey));
