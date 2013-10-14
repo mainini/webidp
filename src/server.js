1c7389df501bf2c52b5afa9dfaec163b867121e3
Pascal Mainini <pascal.mainini@bfh.ch>
2013-10-14 18:30:14 +0200

    bumped version

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
  * @file connect-based node.js-server for WebIDP
  * @copyright 2013 Berne University of Applied Sciences (BUAS) -- {@link http://bfh.ch}
  * @author Pascal Mainini <pascal.mainini@bfh.ch>
- * @version 0.0.1
+ * @version 0.0.2
  *
  * ! WARNING ! WARNING ! WARNING ! WARNING ! WARNING ! WARNING !
  *
