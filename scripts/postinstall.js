f69b3f0c22a6a4cac5446f6bb5103cad894ffd4c
Pascal Mainini <pascal.mainini@bfh.ch>
2013-11-07 18:09:16 +0100

    updated version

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,7 +2,7 @@
  * @file Postinstall-script for npm-package-manager
  * @copyright 2013 Berne University of Applied Sciences (BUAS) -- {@link http://bfh.ch}
  * @author Pascal Mainini <pascal.mainini@bfh.ch>
- * @version 0.0.0
+ * @version 0.0.3
  *
  * ! WARNING ! WARNING ! WARNING ! WARNING ! WARNING ! WARNING !
  *
