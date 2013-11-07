9c06fa2af1574c75978b21d37e0b40aecaa44e4a
Pascal Mainini <pascal.mainini@bfh.ch>
2013-11-07 17:41:42 +0100

    more hacking with backbone.js

--- static/scripts/profile.js
+++ static/scripts/profile.js
@@ -1,46 +1,101 @@
-//$(document).ready(function ()
-//{
+/**
+ * @file Clientside scripting for managing a user's profile
+ * @copyright 2013 Berne University of Applied Sciences (BUAS) -- {@link http://bfh.ch}
+ * @author Pascal Mainini <pascal.mainini@bfh.ch>
+ * @version 0.0.3
+ *
+ * ! WARNING ! WARNING ! WARNING ! WARNING ! WARNING ! WARNING !
+ *
+ * THIS FILE HAS NO DEFINITIVE LICENSING INFORMATION.
+ * LICENSE IS SUBJECT OF CHANGE ANYTIME SOON - DO NOT DISTRIBUTE!
+ *
+ * ! WARNING ! WARNING ! WARNING ! WARNING ! WARNING ! WARNING !
+ *
+ * Based on 
+ * - http://documentcloud.github.io/backbone/docs/todos.html 
+ * - https://github.com/tastejs/todomvc/blob/gh-pages/architecture-examples/backbone/index.html
+ *
+ */
+
+/*jshint jquery:true, bitwise:true, curly:true, immed:true, indent:2, latedef:true, newcap:true, noarg: true, noempty:true, nonew:true, quotmark:single, undef:true, unused: true, trailing:true, white:false */
+/*global document:true, _:true, Backbone: true */
+
+$(document).ready(function readyFunction()
+{
+  // change template tags to avoid conflicts with serverside templating
+  _.templateSettings = {
+    interpolate: /\{\{(.+?)\}\}/g
+  };
+
+//////////////////// models and collections
 
   var WebID = Backbone.Model.extend({});
 
-  var WebIDCollection = Backbone.Collection.extend({
-    url: '/api/webids',
-    model: WebID
+  var WebIDList = Backbone.Collection.extend({
+    model: WebID,
+    url: '/api/webids'
   });
 
-  WebIDView = Backbone.View.extend({
-    el: $('#webidview'),
+  var webids = new WebIDList();
 
-  initialize: function() {
-    this.listenTo(this.model, "change", this.render);
-  },
-//    initialize: function () {
-//      this.listenTo(this.webids, 'change', this.render);
-//    },
 
-    render: function () {
-//      this.$el.html(this.template(this.model.attributes));
-      console.log('Foo: ' + this.webids.length);
-      return this;
+//////////////////// views
+
+  var WebIDView = Backbone.View.extend({
+    tagName: 'li',
+
+    template: _.template($('#webid-template').html()),
+
+    initialize: function initialize() {
+      this.listenTo(this.model, 'change', this.render);
+      this.listenTo(this.model, 'destroy', this.remove);
+      this.render();
+    },
+
+    render: function render() {
+      this.$el.html(this.template(this.model.toJSON()));
     }
   });
 
-  var NavigationRouter = Backbone.Router.extend({
-    initialize: function (options) {
-      this.webids = new WebIDCollection();
+  var WebIDListView = Backbone.View.extend({
+    el: $('#webidlist'),
 
-      this.view = new WebIDView({ model: this.webids });
-      this.listenTo(this.webids, 'change', function () { console.log('ficken') } );
-      this.view.listenTo(this.webids, 'change', this.view.render);
+    initialize: function initialize() {
+      this.listenTo(webids, 'add', this.addOne);
+      this.listenTo(webids, 'reset', this.addAll);
+      this.listenTo(webids, 'all', this.render);
+    },
 
-//      this.webids.reset($('body').data('webids'));
-      this.webids.fetch({ reset: true, 
-                          success: function fetchSuccess(collection, response, options) { console.log('success: ' + JSON.stringify(collection)); },
-                          error: function fetchError(collection, response, options) { console.log('error!'); }, silent:false });
-      console.log('LENGTH: ' + this.webids.length);
+    addOne: function addOne(webid) {
+      var view = new WebIDView({model: webid});
+      this.$el.append(view.el);
+    },
+
+    addAll: function addAll() {
+      webids.each(this.addOne, this);
+    },
+
+    render: function render() {
+      if (webids.length) {
+        this.$el.show();
+      } else {
+        this.$el.hide();
+      }
+    }
+
+  });
+
+  var listView = new WebIDListView();
+  
+//////////////////// Routing
+
+/*  var NavigationRouter = Backbone.Router.extend({
+    initialize: function initialize(options) {
     }
   });
 
   var router = new NavigationRouter();
-  Backbone.history.start();
-//});
+  Backbone.history.start(); */
+
+  webids.reset($('body').data('webids'));   // populate initially by data written to the template and binded to the body-element...
+});
